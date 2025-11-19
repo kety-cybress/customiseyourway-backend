5281df77c7a8c7644f52076e7a48698601a37c3e
Utopianboy99 <madnhlaka@gmail.com>
2025-11-19 11:51:42 +0200

    fixed issues

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT;
-const  URL = process.env.URL_BODY
+const PORT = process.env.PORT || "3001";
+const  URL = process.env.URL_BODY || "23.22.225.132"
 
 let client;
 let db;
