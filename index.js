3bd2d142d62deb58d3918f5c87c24eb62737b65f
Utopianboy99 <madnhlaka@gmail.com>
2025-11-19 14:05:05 +0200

    cors

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "http://www.customiseyourway.com.s3-website-us-east-1.amazonaws.com"
+    "http://www.customiseyourway.com.s3-website-us-east-1.amazonaws.com",
+    "*"
   ],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
